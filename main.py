d62d288695df0b4643e3613e7e5551c244759c50
trentwiles <wilest7@gmail.com>
2024-03-07 15:54:45 +0000

    name

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return render_template("index.html")
+
+@app.route('/submit', methods=["POST"])
+def submit():
+    name = request.args.get("name")
+    reason = request.args.get("reason")
+    amount = request.args.get("amount")
+    email = request.args.get("email")
+
+    print("The person's name was... " + name)
+    return ""
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
