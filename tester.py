0da396f146f6b3419aa276a25fe39bcf0e7c7151
trentwiles <wilest7@gmail.com>
2024-03-07 15:47:37 +0000

    shit logo

--- tester.py
+++ tester.py
@@ -1,3 +1,3 @@
 import pdflib
 
-pdflib.htmlToPDF("<h1>Hey!</h1><p>I am ok</p>", "test.pdf")
+pdflib.htmlToPDF("<center><img src='https://trentwil.es/a/n3xr9QJzE9.png' /></center><h1>Hey!</h1><p>I am ok</p>", "test.pdf")
